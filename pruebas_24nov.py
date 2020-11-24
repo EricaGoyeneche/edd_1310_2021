fb113360c0c5f3d45a5542f5d4f8cc13a725419b
EricaGoyeneche <60679831+EricaGoyeneche@users.noreply.github.com>
2020-11-24 12:46:07 -0600

    Add files via upload

---
+++ pruebas_24nov.py
@@ -0,0 +1,12 @@
+from 24noviembre_1310 import DoubleLinkedList
+
+ld = DoubleLinkedList()
+print('Está vacía?:',ld.is_empty())
+ls.append(10)
+ls.append(20)
+ls.append(30)
+print(f'La lista tiene{ld.get_size()}elementos")
+ld.transversal()
+ld.reverse_transversal()
+ld.remove_from_head(20)
+ld.transversal()
